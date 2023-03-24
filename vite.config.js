9120a90777dc4c32411ada52b873331dc8466934
Christian Alvarenga <>
2023-03-24 15:59:52 -0300

    vite config changed again

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-const mod = import.meta.env.VITE_DEV_MODE;
-
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    server: {
-        host: mod === "prod" ? "0.0.0.0" : "localhost",
-    },
 });
