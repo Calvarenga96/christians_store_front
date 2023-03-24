5542fb4c60e8bf4cf82658083b94830a8b4788c9
Christian Alvarenga <>
2023-03-24 15:20:28 -0300

    fix vite cofig

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+const mod = import.meta.env.VITE_DEV_MODE;
+
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-});
-
-const mod = import.meta.env.VITE_DEV_MODE;
-
-module.exports = {
     server: {
         host: mod === "prod" ? "0.0.0.0" : "localhost",
     },
-};
+});
