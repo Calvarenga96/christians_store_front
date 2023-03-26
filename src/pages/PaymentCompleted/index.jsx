4475abc87fc16187cd1f97fa90d1391cb7774e8c
Christian Alvarenga <>
2023-03-26 12:56:41 -0400

    Change in the websocket

--- src/pages/PaymentCompleted/index.jsx
+++ src/pages/PaymentCompleted/index.jsx
@@ -1,9 +1,21 @@
 import { useEffect, useContext } from "react";
 import { DataContext } from "../../context/DataContext";
+import Echo from "laravel-echo";
+import Pusher from "pusher-js";
+
+window.Pusher = Pusher;
+
+const echo = new Echo({
+    broadcaster: "pusher",
+    key: import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    forceTLS: true,
+});
 
 const devMode = import.meta.env.VITE_DEV_MODE;
+
 const uri =
-    "devMode" === "dev"
+    devMode === "dev"
         ? "http://127.0.0.1:6001"
         : "https://christiansstoreback-production.up.railway.app";
 
@@ -11,10 +23,10 @@ export function PaymentCompleted() {
     const { docId } = useContext(DataContext);
 
     useEffect(() => {
-        Echo.channer("payment-status-update").listen(
+        echo.channel(`payment-status-update`).listen(
             "PaymentStatusUpdated",
             (e) => {
-                console.log(e);
+                console.log(e.event);
             }
         );
     }, []);
